c984bc7273525be70a650ba596601ced8ba08f1f
BenjaminLucas313 <benjaminlucassgmt@gmail.com>
2026-04-26 22:16:17 -0300

    feat: Sentry configurado + ruta test eliminada

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn:               process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate:  0.1,
-  debug:             false,
-  enabled:           process.env.NODE_ENV === "production",
+  dsn: "https://25bf6770dcd949cbc91faeec21b01b8d@o4511289220202496.ingest.us.sentry.io/4511289230557184",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
